3a28f478c1df209e1fa3c6e9b4b147d174ae170b
YOONSU PARK <shinebytheriver@yahoo.com>
2019-04-25 08:36:51 -0700

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Clock from './Clock';
+import Toggle from './Toggle';
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//     ReactDOM.render(
+//       element,
+//       document.getElementById('root')
+//     );
+//   }
+  
+//   setInterval(tick, 1000);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function Clock(props) {
+//     return (
+//       <div>
+//         <h1>Hello, world with clock()!</h1>
+//         <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+  
+//   function tick() {
+//     ReactDOM.render(
+//       <Clock date={new Date()} />,
+//       document.getElementById('root')
+//     );
+//   }
+  
+//   setInterval(tick, 1000);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/****** func -> class  ******/
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <Clock />;
+  }
+  return <Toggle />;
+}
+
+  ReactDOM.render(
+    // <Clock />,
+    // <Toggle />,
+    <Greeting isLoggedIn = {true} />,
+    document.getElementById('root')
+  );
